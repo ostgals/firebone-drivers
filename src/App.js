b7a84cbb5ec75c75889cacdc12ec6751b06c9be8
Andrejs Kuzmins <ostgals@gmail.com>
2019-03-07 15:19:11 +0200

    basic functionality

--- src/App.js
+++ src/App.js
@@ -1,28 +1,77 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  let [target, setTarget] = useState('');
+  let [version, setVersion] = useState(1);
+  let [oid, setOid] = useState('');
+
+  let [error, setError] = useState(null);
+
+  let [subtree, setSubtree] = useState([]);
+  let [changed, setChanged] = useState([]);
+
+  let fetchSubtree = useCallback(() => {
+    let body = JSON.stringify({
+      target,
+      options: { version },
+      oid,
+    });
+
+    fetch('http://localhost:4444/subtree', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body,
+    })
+      .then(res => res.json())
+      .then(setSubtree)
+      .catch(setError);
+  }, [target, version, oid]);
+
+  return (
+    <div>
+      <h1>Firebone Drivers</h1>
+
+      <section>
+        <h2>Session</h2>
+        <fieldset>
+          <label>
+            Host:
+            <input value={target} onChange={e => setTarget(e.target.value)} />
+          </label>
+          <label>
+            Version:
+            <select value={version} onChange={e => setVersion(+e.target.value)}>
+              <option value={0}>SNMP v1</option>
+              <option value={1}>SNMP v2c</option>
+            </select>
+          </label>
+        </fieldset>
+      </section>
+
+      <section>
+        <h2>Subtree</h2>
+        <fieldset>
+          <label>
+            Root OID:
+            <input value={oid} onChange={e => setOid(e.target.value)} />
+          </label>
+          <button onClick={fetchSubtree}>Fetch</button>
+
+          {error && <p>{error.message || error}</p>}
+        </fieldset>
+
+        <ul className="varbinds">
+          {subtree.map(varbind => (
+            <li key={varbind.oid}>
+              <span className="oid">{varbind.oid}</span>
+              <span className="type">{varbind.type}</span>
+              <span className="value">{varbind.value}</span>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+};
 
 export default App;
